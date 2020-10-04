8938f486a3927e88cda7daf213f8ed064372d087
czoubkov <42822917+czoubkov@users.noreply.github.com>
2020-10-03 22:59:28 -0400

    Completed project 1 and 2

--- zip-search/src/App.js
+++ zip-search/src/App.js
@@ -3,25 +3,112 @@ import './App.css';
 
 
 function City(props) {
-  return (<div>This is the City component</div>);
+  return (
+    <div className="card" style="width: 18rem;">
+    <div className="card-body">
+      <h3 class="card-title">{props.data.City}, {props.data.State}</h3>
+      <ul>
+        <li>Population: {props.data.EstimatedPopulation}</li>
+        <li>Location: ({props.data.Lat}, {props.data.Long})</li>
+      </ul>
+    </div>
+  </div>
+  );
+}
+
+function Zip(props) {
+  return (
+    <span>{props.data}, </span>
+  );
 }
 
 function ZipSearchField(props) {
-  return (<div>This is the ZipSearchField component</div>);
+  return (
+    <div>
+      Zip Code:
+      <input type="text" onChange={ props.zipChanged } value={ props.zipValue }/>
+      <p>You entered: { props.zipValue }</p>
+    </div>);
 }
 
+function CitySearchField(props) {
+  return (
+    <div>
+      City Name:
+      <input type="text" onChange={ props.cityChanged } value={ props.cityValue }/>
+      <p>You entered: { props.cityValue }</p>
+    </div>
+  )
+}
 
 class App extends Component {
+  state = {
+    inputZip: "",
+    cityResults: [],
+    inputCity: "",
+    zipResults: []
+  }
+
+  handleZipChange(event) {
+    this.setState({
+      inputZip: event.target.value
+    })
+
+    if(event.target.value.length === 5) {
+      fetch('https://ctp-zip-api.herokuapp.com/zip/' + event.target.value)
+        .then(res => res.json())
+        .then(jsonData => {
+          this.setState({
+            cityResults: jsonData,
+          })
+        })
+        .catch(err => {
+          this.setState({ cityResults: [] })
+        })
+    } else {
+      this.setState({ cityResults: [] })
+    }
+  }
+
+  handleCityChange(event) {
+    this.setState({
+      inputCity: event.target.value
+    })
+
+    fetch('https://ctp-zip-api.herokuapp.com/city/' + event.target.value.toUpperCase())
+      .then(res => res.json())
+      .then(jsonData => {
+        this.setState({
+          zipResults: jsonData,
+        })
+      })
+      .catch(err => {
+        this.setState({ zipResults: [] })
+      })
+  }
+
   render() {
+    // Zip code search returns city results
+    console.log(this.state.cityResults);
+    // City search returns zip code results
+    console.log(this.state.zipResults);
+
     return (
       <div className="App">
         <div className="App-header">
           <h2>Zip Code Search</h2>
         </div>
-        <ZipSearchField />
+        <ZipSearchField zipChanged={(e) => this.handleZipChange(e)} zipValue={this.state.inputZip} />
         <div>
-          <City />
-          <City />
+          { this.state.cityResults.map((item, index) => {
+            return <City data={item} key={index}/>
+          }) }
+        </div>
+        <CitySearchField cityChanged={(e) => this.handleCityChange(e)} cityValue={this.state.inputCity} />
+        <div>
+          { this.state.zipResults.map((item, index) => {
+            return <Zip data={item} key={index}/>
+          }) }
         </div>
       </div>
     );
